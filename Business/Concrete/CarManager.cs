0f91dffc16ad91a0bd44e5b6d6d6bff5af3b76c1
kaankaplan111@gmail.com <kaankaplan111@gmail.com>
2021-03-02 00:52:12 +0300

    2 Mart 2021

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,9 +1,12 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -20,13 +23,12 @@ namespace Business.Concrete
 
         public IResult Add(Car car)
         {
-            if (car.CarName.Length >= 2 && car.DailyPrice > 0)
-            {
-                _carDal.Add(car);
+            ValidationTool.Validate(new CarValidator() ,car);
 
-                return new SuccessResult(Message.SuccessfullyAdded);
-            }
-            return new ErrorResult(Message.CarNameAndPriceError);
+            _carDal.Add(car);
+
+            return new SuccessResult(Message.SuccessfullyAdded);
+           
         }
 
         public IResult Delete(Car car)
--- Business/Concrete/RentalManager.cs
+++ Business/Concrete/RentalManager.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -18,10 +20,8 @@ namespace Business.Concrete
         }
         public IResult Add(Rental rental)
         {
-            if (rental.ReturnDate == null)
-            {
-                return new ErrorResult(Message.ErrorReturnDateNull);
-            }
+            ValidationTool.Validate(new RentalValidator(), rental);
+
             _rentalDal.Add(rental);
 
             return new SuccessResult(Message.SuccessfullyRented);
